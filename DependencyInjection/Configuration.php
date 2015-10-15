e9d7acba91ca30d52c5ba1329e45581c9cee250c
igor <igor.sv.n@gmail.com>
2015-10-15 17:47:56 +0300

    Remove redundant files.

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -18,7 +18,7 @@ class Configuration implements ConfigurationInterface
     public function getConfigTreeBuilder()
     {
         $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('darvin_bot_detector');
+//        $rootNode = $treeBuilder->root('darvin_bot_detector');
 
         // Here you should define the parameters that are allowed to
         // configure your bundle. See the documentation linked above for
