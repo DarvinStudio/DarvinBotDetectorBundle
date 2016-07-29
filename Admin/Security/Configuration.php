22dfc43f174f77c7b9a71fe504035ec95f9bba39
igor <igor.sv.n@gmail.com>
2016-07-29 11:25:20 +0300

    Move admin security configuration service classes to "Admin/Security" namespace.

--- Admin/Security/Configuration.php
+++ Admin/Security/Configuration.php
@@ -8,15 +8,15 @@
  * file that was distributed with this source code.
  */
 
-namespace Darvin\BotDetectorBundle\Security;
+namespace Darvin\BotDetectorBundle\Admin\Security;
 
 use Darvin\AdminBundle\Security\Configuration\AbstractSecurityConfiguration;
 use Darvin\BotDetectorBundle\Entity\DetectedBot;
 
 /**
- * Security configuration
+ * Admin security configuration
  */
-class SecurityConfiguration extends AbstractSecurityConfiguration
+class Configuration extends AbstractSecurityConfiguration
 {
     /**
      * {@inheritdoc}
--- DependencyInjection/DarvinBotDetectorExtension.php
+++ DependencyInjection/DarvinBotDetectorExtension.php
@@ -23,7 +23,11 @@ class DarvinBotDetectorExtension extends Extension
 //        $config = $this->processConfiguration($configuration, $configs);
 
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
-        $loader->load('admin.yml');
-        $loader->load('security.yml');
+
+        foreach ([
+            'admin',
+        ] as $resource) {
+            $loader->load($resource.'.yml');
+        }
     }
 }
