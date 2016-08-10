04f15693e7f543dbf7f29f2031d983056b4fcc6a
igor <igor.sv.n@gmail.com>
2016-08-10 11:41:42 +0300

    Adapt to new admin bundle version.

--- DependencyInjection/DarvinBotDetectorExtension.php
+++ DependencyInjection/DarvinBotDetectorExtension.php
@@ -2,8 +2,9 @@
 
 namespace Darvin\BotDetectorBundle\DependencyInjection;
 
-use Symfony\Component\Config\FileLocator;
+use Darvin\BotDetectorBundle\Entity\DetectedBot;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\PrependExtensionInterface;
 use Symfony\Component\DependencyInjection\Loader;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 
@@ -12,7 +13,7 @@ use Symfony\Component\HttpKernel\DependencyInjection\Extension;
  *
  * To learn more see {@link http://symfony.com/doc/current/cookbook/bundles/extension.html}
  */
-class DarvinBotDetectorExtension extends Extension
+class DarvinBotDetectorExtension extends Extension implements PrependExtensionInterface
 {
     /**
      * {@inheritDoc}
@@ -22,12 +23,31 @@ class DarvinBotDetectorExtension extends Extension
 //        $configuration = new Configuration();
 //        $config = $this->processConfiguration($configuration, $configs);
 
-        $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+//        $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
 
-        foreach ([
-            'admin',
-        ] as $resource) {
-            $loader->load($resource.'.yml');
+//        foreach ([
+//        ] as $resource) {
+//            $loader->load($resource.'.yml');
+//        }
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function prepend(ContainerBuilder $container)
+    {
+        $bundles = $container->getParameter('kernel.bundles');
+
+        if (isset($bundles['DarvinAdminBundle'])) {
+            $container->prependExtensionConfig('darvin_admin', [
+                'sections' => [
+                    [
+                        'alias'  => 'bot',
+                        'entity' => DetectedBot::DETECTED_BOT_CLASS,
+                        'config' => __DIR__.'/../Resources/config/admin/detected_bot.yml',
+                    ],
+                ],
+            ]);
         }
     }
 }
